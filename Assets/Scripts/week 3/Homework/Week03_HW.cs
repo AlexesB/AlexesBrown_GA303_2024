d1c201b4bb0a202e8dbba95a3f29752e28ea3846
AlexesB <alexesbrown@lcad.edu>
2024-10-04 08:08:16 -0700

    Not sure
    Don't knwo

--- Assets/Scripts/week 3/Homework/Week03_HW.cs
+++ Assets/Scripts/week 3/Homework/Week03_HW.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Week03_HW : MonoBehaviour
 {
+    public GameObject spawnedPrefab;
+
+    public Transform spawner;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,19 @@ public class Week03_HW : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GameObject Tom = Instantiate(spawnedPrefab, spawner.position, spawner.rotation);
+            Tom.GetComponent<Light>().color = Color.cyan;
+
+        }
+
+    }
+
+    public void SpawnTom()
+    {
+        GameObject Tom = Instantiate(spawnedPrefab, this.transform.position, this.transform.rotation);
+
+        Tom.GetComponent<Light>().color = Color.red;
     }
 }
